08bac17e53946400d20d324f2e23630ac61a5e88
wysiwyg marketing <wysiwygseo@gmail.com>
2021-03-23 13:39:12 +0530

    We fixed the counter score

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-    $("input[type='checkbox']").click(function(){ 
+    $(".checked--one").click(function(){ 
         updateFunction(); 
+    });
+
+    $(".checked--two").click(function(){ 
+        updateFunctionone(); 
+    });
+
+    $(".checked--three").click(function(){ 
+        updateFunctiontwo(); 
     });
 
     function updateFunction(){
         $(".ckeck-one-reslt").text($(".checked--one:checked").length);
+    }
+
+    function updateFunctionone(){
+        $(".ckeck-two-reslt").text($(".checked--two:checked").length);
+    }
+
+    function updateFunctiontwo(){
+        $(".ckeck-three-reslt").text($(".checked--three:checked").length);
     }
 });
